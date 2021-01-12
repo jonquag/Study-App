d5d5c494da48d0ed52d087da9120ce7e5df22d65
amha19 <34722096+amha19@users.noreply.github.com>
2021-01-11 18:55:22 -0600

    Added routes to an empty signup and login pages.
    The landing and any other unknown pages temporarily redirects to login.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,45 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import { theme } from './themes/theme';
+// import LandingPage from './pages/Landing';
+import Signup from './pages/auth/Sign-up';
+import Login from './pages/auth/Login';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+    return (
+        <MuiThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Switch>
+                    {/* Landing page temporarily redirects to login. */}
+                    <Route
+                        exact
+                        path="/"
+                        render={() => <Redirect to="/login" />}
+                    />
+
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/sign-up" component={Signup} />
+
+                    {/* Any other unknown paths redirects to login. */}
+                    <Route
+                        path="*"
+                        render={props => (
+                            <Redirect
+                                to={{
+                                    pathname: '/login',
+                                    state: { from: props.location },
+                                }}
+                            />
+                        )}
+                    />
+                </Switch>
+            </BrowserRouter>
+        </MuiThemeProvider>
+    );
 }
 
 export default App;
