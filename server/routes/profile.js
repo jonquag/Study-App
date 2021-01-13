7b7297044db82494242e0f905e96aaf5b761b3a1
Ethan Rosenberg <ethan@ethanrosenberg.com>
2021-01-13 15:32:34 -0800

    profile.js route returns and updates Profile model
    updates user Profile model firstName, lastName, phone and location fields. References User model by ID and adds respective email to response.

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const auth = require('../middleware/verify-auth');
 
 const Profile = require('../models/Profile');
+const { update } = require('../models/user');
 const User = require('../models/user');
 
+// Returns current user profile based on ID
 router.get('/:user_id', auth, async (req, res) => {
     try {
-        
-    const profile = await Profile.findOne({ user: req.params.user_id });
+    //find profile by user id and populate email
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate('User', ['email']);
 
     if(!profile) {
         return res.status(400).json({ msg: "No profile found." });
     }
+
     res.json(profile);
 
     } catch(err){
@@ -23,6 +26,36 @@ router.get('/:user_id', auth, async (req, res) => {
     res.status(500).send('Server Error');
     }
 
+})
+
+//Updates user profile fields based on ID and populates email
+router.post('/:user_id', auth, async (req, res) => {
+    try {
+        //find profile
+        let profile = await Profile.findOne({ user: req.params.user_id })
+        const { firstName, lastName, phone, location } = req.body;
+
+        //if profile found then update
+        if(profile) {
+            const filter = { user: req.params.user_id };
+            const updatedProfile = {};
+            if(firstName) updatedProfile.firstName = firstName;
+            if(lastName) updatedProfile.lastName = lastName;
+            if(phone) updatedProfile.phone = phone;
+            if(location) updatedProfile.location = location;
+
+            //update profile and populate email
+            profile = await Profile.findOneAndUpdate(filter, updatedProfile,  { new: true } ).populate('User', ['email'])
+
+            return res.json(profile);
+        }
+        //return error message if no profile found
+        return res.status(400).json({ msg: "No profile found." });
+        
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send("Server error!");
+    }
 })
 
 module.exports = router;
