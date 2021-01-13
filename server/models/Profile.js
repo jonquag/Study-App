bd8d5f5afa29ad3e20fce2b7a00186eb6c08f41b
Ethan Rosenberg <ethan@ethanrosenberg.com>
2021-01-13 10:35:41 -0800

    Created Profile Model
    referencing user by ID along with fields, firstName, lastName, email, phone, location.

---
+++ server/models/Profile.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Create schema for profile fields
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    location: {
+        type: String
+    }
+});
+
+// create Profile model with ProfileSchema and export module
+module.exports = mongoose.model('Profile', ProfileSchema);
