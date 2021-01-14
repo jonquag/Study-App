3e3a05aff5adee3a3b475cf801684d1e05a8f676
Ethan Rosenberg <ethan@ethanrosenberg.com>
2021-01-14 12:13:05 -0800

    check profile update values for null values

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/verify-auth');
 
-const Profile = require('../models/Profile');
-const { update } = require('../models/user');
+const Profile = require('../models/profile');
 const User = require('../models/user');
 
 // Returns current user profile based on ID
@@ -11,7 +10,7 @@ router.get('/:user_id', auth, async (req, res) => {
     try {
 
     //find profile by user id and populate email
-    const profile = await Profile.findOne({ user: req.body.userId }).populate('User', ['email']);
+    const profile = await Profile.findOne({ user: req.body.userId }).populate('user', ['email']);
 
     if(!profile) {
         return res.status(400).json({ msg: "No profile found." });
@@ -40,13 +39,13 @@ router.put('/:user_id', auth, async (req, res) => {
         if(profile) {
             const filter = { user: req.body.userId };
             const updatedProfile = {};
-            if(firstName) updatedProfile.firstName = firstName;
-            if(lastName) updatedProfile.lastName = lastName;
-            if(phone) updatedProfile.phone = phone;
-            if(location) updatedProfile.location = location;
-
+            if (firstName !== null) updatedProfile.firstName = firstName;
+            if (lastName !== null) updatedProfile.lastName = lastName;
+            if (phone !== null) updatedProfile.phone = phone;
+            if (location !== null) updatedProfile.location = location;
+            
             //update profile and populate email
-            profile = await Profile.findOneAndUpdate(filter, updatedProfile,  { new: true } ).populate('User', ['email'])
+            profile = await Profile.findOneAndUpdate(filter, updatedProfile,  { new: true } ).populate('user', ['email'])
 
             return res.json(profile);
         }
