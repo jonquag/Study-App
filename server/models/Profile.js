12c1e16d6cfc1c675aaba8b431ed232d33fc8c4d
Ethan Rosenberg <ethan@ethanrosenberg.com>
2021-01-14 12:19:15 -0800

    Update Profile.js

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const ProfileSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+        ref: 'User'
     },
     firstName: {
         type: String
