a55ff197454de3918a582725627f0e041d2a0150
Ethan Rosenberg <ethan@ethanrosenberg.com>
2021-01-14 22:32:17 -0800

    uploads image to cloudinary and returns hosted image url

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,11 +1,48 @@
 const express = require('express');
-var multer = require('multer');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier'); 
 const router = express.Router();
-
 const fileUpload = multer();
 
-router.post("/", async function(req, res) {
+//Get cloud_name, api_key and api_secret from .env
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+//stream file upload
+router.post("/", fileUpload.single("image"), async function(req, res) {
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+              (error, result) => {
+                if (result) {
+                  resolve(result);
+                } else {
+                  reject(error);
+                }
+              }
+            );
+    
+           streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    }
+
+    //upload and get response
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result.url);
+        res.send(result.url)
+    }
+
+    upload(req);
 
 })
+
+
+
+
 
 module.exports = router;
