74fce628c6841979360fe264ce24945ea6b52067
Ethan Tam <ethantam@reelable.social>
2021-01-19 09:06:56 -0500

    Keep create course and university routes

--- server/routes/universities.js
+++ server/routes/universities.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const { BadRequest, GeneralError } = require('../utils/errors');
+const Course = require("../models/courses");
 const University = require('../models/universities');
-require("../models/courses");
-
 
 //GET all universities with populated courses
 router.get("/", async function(req, res, next) {
@@ -20,6 +19,31 @@ router.get('/:universityId/courses', async function(req, res, next) {
     const university = await University.findById({ _id: universityId })
     .populate({path: 'courses', model: 'Course'});
     res.status(200).send(university);
+});
+
+//POST create a new course and update university with the course id
+router.post('/courses', async function(req, res, next) {
+    const course = new Course(req.body);
+    const {universityId} = req.body;
+    if (!universityId) return next(new BadRequest('Missing required field'));
+    try {
+        const courseDoc = await course.save();
+        await University.findByIdAndUpdate(req.body.universityId, { "$push": { "courses": courseDoc.id } })
+        res.sendStatus(201);
+    } catch (err) {
+       next(new GeneralError('Server Error'));
+    }
+})
+
+//POST create a new university
+router.post('/', async function (req, res, next) {
+    try {
+        const university = new University(req.body);
+        await university.save();
+        res.sendStatus(200);
+    } catch (err) {
+        next(new GeneralError('Server Error'));
+    }
 });
 
 module.exports = router;
