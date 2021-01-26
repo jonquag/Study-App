4ecc10ec613bb95838cab63d1b0e157c098d8506
Ethan Rosenberg <ethan@ethanrosenberg.com>
2021-01-25 23:45:32 -0800

    added Create New Group button with modal popup

--- client/src/pages/contentPages/Groups.js
+++ client/src/pages/contentPages/Groups.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Grid, Typography, Container, Button } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Grid, Typography, Container, Button, Modal } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { groups } from '../../data/mockData.js'
 import Navbar from '../layout/Navbar';
 import GroupCard from '../../components/Group/GroupCard';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -18,12 +19,74 @@ const useStyles = makeStyles((theme) => ({
       marginRight: "60px",
       textTransform: 'none',
       height: '3rem',
-  }
+  },
+    paper: {
+      position: 'fixed',
+      width: 475,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
     
   }));
 
+  function getModalStyle() {
+    const top = 50;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
 const Groups = () => {
+
     const classes = useStyles();
+
+    const [modalStyle] = React.useState(getModalStyle);
+    const [openModal, setOpen] = useState(false);
+    const [courses, setCourses] = useState([]);
+
+     // fetch all the users courses
+     const fetchUserCourses = async () => {
+      try {
+          const response = await axios.get('/user/courses');
+
+          setCourses(response.data);
+
+      } catch (err) {
+          console.log(err);
+      }
+  };
+
+  useEffect(() => {
+    fetchUserCourses();
+  }, []);
+
+    const body = (
+      <div style={modalStyle} className={classes.paper}>
+      <h2>Create a new group</h2>
+      <p>
+        Add group name and select your course.
+        {
+          courses.map(course => {
+            return <p>{course.name} </p>
+          })
+        }
+      </p>
+    </div>
+    );
+
+    const handleOpen = () => {
+      setOpen(true)
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
 
     return (
         <Grid>
@@ -41,7 +104,7 @@ const Groups = () => {
             direction="row"
             container
             md={4}>
-                <Typography variant="h1" color="textPrimary" style={{ paddingTop: "60px"}}>
+               <Typography variant="h1" color="textPrimary" style={{ paddingTop: "60px"}}>
                     Sugggested For You.
                 </Typography>
                 <Typography variant="h6" color="textSecondary" style={{ paddingTop: "20px"}}>
@@ -54,9 +117,18 @@ const Groups = () => {
             direction="row"
             container
             md={4}>
-                 <Button  className={classes.button}>
-                      Create New Group
-                 </Button>
+              <Button
+                className={classes.button}
+                onClick={handleOpen}
+                >
+                Create New Group
+              </Button>
+              <Modal
+                open={openModal}
+                onClose={handleClose}
+                >
+                {body}
+            </Modal>
             </Grid>       
         </Grid>
 
