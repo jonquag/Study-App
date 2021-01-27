34f6408b249295879f6291c3010968c43ec1704d
Ethan Tam <ethantam@reelable.social>
2021-01-27 09:17:43 -0500

    remove unused styles

--- client/src/pages/contentPages/Groups.js
+++ client/src/pages/contentPages/Groups.js
@@ -6,33 +6,7 @@ import axios from 'axios';
 import { useGlobalContext } from '../../context/studyappContext';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-      border: 'solid',
-    },
-    gridList: {
-      flexWrap: 'nowrap',
-      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-      transform: 'translateZ(0)',
-    },
-    title: {
-      color: theme.palette.primary.light,
-    },
-    titleBar: {
-      background:
-        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
     cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
-    hozirontalCardGrid: {
-      flexWrap: 'nowrap',
-      transform: 'translateZ(0)',
       paddingTop: theme.spacing(8),
       paddingBottom: theme.spacing(8),
     },
