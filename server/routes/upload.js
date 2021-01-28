a4b105cd7d58397e75798546781c77fa73d645ab
Ethan Rosenberg <ethan@ethanrosenberg.com>
2021-01-28 15:39:27 -0800

    added /single image upload route to return hosted image url

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -5,8 +5,19 @@ const router = express.Router();
 const fileUpload = multer();
 const streamUpload = require('../controllers/upload');
 const updateProfile = require('../controllers/profile');
+const { response } = require('express');
+
 
 //stream file upload
 router.post("/", fileUpload.single("image"), auth, [streamUpload, updateProfile]);
+
+//stream image upload and return hosted url
+router.post("/single", fileUpload.single("image"), auth, [streamUpload, (req, res, next) => {
+    try {
+        res.status(201).json(req.body);
+    } catch(err) {
+        next(err);
+    }
+}])
 
 module.exports = router;
