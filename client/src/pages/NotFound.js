0bddb7df70e80fb9963535f3ae3c7a01cba56276
Ethan Tam <ethantam@reelable.social>
2021-01-29 10:40:25 -0500

    FE-redirects-59

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import { useGlobalContext } from '../context/studyappContext';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -14,11 +16,19 @@ const useStyles = makeStyles(theme => ({
         width: '100vw'
     },
     icon: { fontSize: '300px' },
-    text: { paddingTop: "60px" },
+    text: { opacity: '80%', padding: '1em'},
+    linkStyles: {
+        textDecoration: 'none',
+        color: 'blue',
+        padding: '1em 0 0 2em',
+        fontSize: 15,
+        opacity: '50%',
+    },
 }));
 
 const NotFound = () => {
     const classes = useStyles();
+    const { isAuth } = useGlobalContext();
 
     return (
         <div className={classes.root}>
@@ -29,6 +39,10 @@ const NotFound = () => {
             <Typography variant="h6" color="textPrimary" className={classes.text}>
                 The page you are looking for doesn't exist or another error occured.
             </Typography>
+            <Box flexDirection='row' className={classes.linkContainer}>
+                {isAuth && <NavLink to='/profile' className={classes.linkStyles}>Profile</NavLink>}
+                <NavLink to='/login' className={classes.linkStyles}>Login</NavLink>
+            </Box>
         </div>
     )
 };
