7c42e899ee1e1a55666b833497d19e7262cd7b5c
amha19 <34722096+amha19@users.noreply.github.com>
2021-02-02 10:44:19 -0600

    Fixed error handlign for current password

--- client/src/utils/handleAuthErrors.js
+++ client/src/utils/handleAuthErrors.js
@@ -8,6 +8,7 @@ const handleAuthErrors = (err, setErrors) => {
             setErrors(errors);
         } else if (err.status === 401) {
             setErrors({ password: 'Invalid password' });
+            setErrors({ oldPassword: 'Invalid password' });
         } else if (err.status === 404) {
             setErrors({ email: 'User with email not found' });
         } else if (err.status === 409) {
