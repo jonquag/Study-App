193d20566adcb7ff0124e08c59ac9dd8a6c18466
Ethan Rosenberg <ethan@ethanrosenberg.com>
2021-02-04 20:38:26 -0800

    added forum get posts route

--- server/routes/forum.js
+++ server/routes/forum.js
@@ -4,6 +4,8 @@ const { check } = require('express-validator');
 const auth = require('../middleware/verifyAuth');
 const postController = require('../controllers/post');
 const commentController = require('../controllers/comment');
+const Group = require('../models/Group');
+const Forum = require('../models/Forum');
 
 const router = express.Router();
 
@@ -59,5 +61,43 @@ router.put(
 // PUT forum/post/comment/:postId
 // Edit a comment using comment id
 router.put('/post/comment/:commentId', auth, commentController.editComment);
+
+// returns all the groups a user can join from their courses
+router.get('/:groupId', auth, async function (req, res, next) {
+//console.log(req.params.groupId)
+    try {
+        // start new session for transaction
+        const session = await Group.startSession();
+        session.startTransaction();
+
+        const userDoc = await Group.findById(req.params.groupId)
+            .populate({
+                path: 'forums',
+                model: 'Forum',
+    
+            })
+            .session(session)
+            .catch(() => {
+                throw new GeneralError('Error returning group forum');
+            });
+            const forumDoc = await Forum.findById({ _id: userDoc.forum })
+            .populate({
+                path: 'posts',
+                model: 'Post',
+    
+            })
+            .catch(() => {
+                throw new GeneralError('Error returning forum posts');
+            });
+        if (forumDoc && forumDoc.posts) {
+            res.send(forumDoc);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
 
 module.exports = router;
